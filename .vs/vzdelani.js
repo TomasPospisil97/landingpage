9ff46ed1e5f809dc87762c1260ab7713a6033612
TomasPospisil97 <tp.tomaspospisil@gmail.com>
2024-05-28 19:07:25 +0200

    added flappy and calculator option in education for now

--- .vs/vzdelani.js
+++ .vs/vzdelani.js
@@ -1,1 +1,24 @@
 alert ('Here you can find my education');
+
+const ACTIVECLASS = 'active';
+const IMAGES = document.querySelectorAll('.flex-card-container');
+
+IMAGES[0].classList.add(ACTIVECLASS);
+
+function removeActiveClass() {
+    const elm = document.querySelector(`.${ACTIVECLASS}`);
+    if (elm) {
+        elm.classList.remove(ACTIVECLASS);
+    }
+}
+
+function addClass($event) {
+    $event.stopPropagation();
+    removeActiveClass();
+    const target = $event.currentTarget;
+    target.classList.add(ACTIVECLASS);
+}
+
+IMAGES.forEach(image => {
+    image.addEventListener('click', addClass);
+});
