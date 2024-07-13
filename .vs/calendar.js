b56ec3f753d1ee2fbf66ae18d83022794b12d726
TomasPospisil97 <tp.tomaspospisil@gmail.com>
2024-07-13 19:21:26 +0200

    left side logic done

--- .vs/calendar.js
+++ .vs/calendar.js
@@ -1,8 +1,14 @@
+//const pro kalendar
 const calendar = document.querySelector(".calendar");
 const date = document.querySelector(".date");
 const daysContainer = document.querySelector(".days");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
+
+//const pro button left side
+const todayBtn = document.querySelector(".today-btn");
+const gotoBtn = document.querySelector(".goto-btn");
+const dateInput = document.querySelector(".date-input");
 
 
 let today = new Date();
@@ -25,7 +31,7 @@ const months = [
     "December"
 ];
 
-//now we will add days
+//pridani dnu
 
 function initCalendar(){
     const firstDay = new Date(year, month, 1);
@@ -38,30 +44,30 @@ function initCalendar(){
 
     date.innerHTML = months[month] + " " + year;
 
-    //adding days
+    //pridavani dnu
 
     let days = "";
 
-    //prev months
+    //dny z predchoziho mesice
 
     for (let x = day ; x > 0 ; x--){
         days += `<div class="day prev-date" >${prevDays - x + 1}</div>`;
     }
 
-    //current month days
+    //dny v tomto mesici
 
     for (let i = 1 ; i<=lastDate; i++){
-        //if day is today and class today
+        //day is today + class today
         if(i === new Date().getDate() && year === new Date().getFullYear() && month === new Date().getMonth()){
             days += `<div class="day today">${i}</div>`
-        }//remaining
+        }//zbytek
         else{
             days += `<div class="day">${i}</div>`
         }
 
     }
 
-    //next month
+    //dalsi mesic
     for (let j = 1 ; j<=nextDays ; j++){
         days += `<div class="day next-date">${j}</div>`
     }
@@ -71,3 +77,80 @@ function initCalendar(){
 };
 
 initCalendar();
+
+
+//predchozi mesic
+
+function prevMonth(){
+    month --;
+    if(month < 0){
+        month = 11;
+        year --;
+    }
+    initCalendar();
+}
+
+//nasledujici mesic
+
+function nextMonth(){
+    month ++;
+    if(month > 11){
+        month = 0;
+        year ++;
+    }
+    initCalendar();
+}
+
+//eventlistener pro predchozi a dalsi mesic
+
+prev.addEventListener("click" , prevMonth);
+next.addEventListener("click" , nextMonth);
+
+
+//goto + goto today funkce
+
+todayBtn.addEventListener("click" , ()=> {
+    today = new Date();
+    month = today.getMonth();
+    year = today.getFullYear();
+    initCalendar();
+});
+
+dateInput.addEventListener("input" , ()=>{
+    //pouze pro cisla
+    dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+    if(dateInput.value.length === 2){
+        //po 2 cislech prida lomitko
+        dateInput.value += "/";
+    }
+    if(dateInput.value.length > 7){
+        //nepripusti vic jak 7 charakteru = vcetne lomitka
+        dateInput.value = dateInput.value.slice(0, 7);
+    }
+    //nesmaze se vice jak 2 prvni charaktery
+    //if(e.inputType === "deleteContentBackward"){
+      //  if(dateInput.value.length === 3){
+        //    dateInput.value = dateInput.value.slice(0, 2);
+        //}
+    //}
+});
+
+gotoBtn.addEventListener("click" , gotoDate);
+
+//goto urcene datum
+
+function gotoDate(){
+    const dateArr = dateInput.value.split("/");
+    //date validace
+    if(dateArr.length === 2){
+        if(dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4){
+            month = dateArr[0] - 1;
+            year = dateArr[1];
+            initCalendar();
+            return;
+        }
+    }
+
+    //pokud je spatne datum
+    alert("Invalid date");
+}
